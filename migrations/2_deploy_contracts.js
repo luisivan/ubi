1fdd44dd346ab31c0e8b4d04b33370e5f0c55668
Luis Cuende <me@luisivan.net>
2019-04-30 16:16:19 +0200

    Renamed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 /* global artifacts */
-var CounterApp = artifacts.require('CounterApp.sol')
+var UBI = artifacts.require('UBI.sol')
 
 module.exports = function(deployer) {
-  deployer.deploy(CounterApp)
+  deployer.deploy(UBI)
 }
